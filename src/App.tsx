a1c704ee5570f1b534cfdbdeeef47b3e31cb1e7c
songhajang <angpan2004@gmail.com>
2025-09-11 19:34:00 +0900

    feat: 패키지 vite으로 수정 + tailwindcss 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='w-full h-full flex flex-col items-center justify-center'>
+        <h1>프론트 메인입니다. </h1>
+        추후 여기 부분 건드는 사람은 장송하 일 뿐 ㅎㅎ
+        좋아요~~ <strong>{count}</strong>
+        <br />
+        <button onClick={() => { setCount(count + 1) }}> 좋아요 버튼</button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
