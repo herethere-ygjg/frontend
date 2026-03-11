3c599af1bebfdcb6a1bb0c36b0c4c059c88ae61e
songhajang <angpan2004@gmail.com>
2026-03-11 16:49:04 +0900

    [add] #1 - div props 추가

--- src/components/layout/DefaultDiv.tsx
+++ src/components/layout/DefaultDiv.tsx
@@ -3,13 +3,14 @@ import type { ReactNode } from "react";
 interface DefaultDivProps {
   children: ReactNode
   isBackgroundColor?: boolean
+  className? : string
 }
 
 
-const DefaultDiv = ({children , isBackgroundColor = false} : DefaultDivProps) =>{
+const DefaultDiv = ({children , isBackgroundColor = false, className = ""} : DefaultDivProps) =>{
     return(
         <div 
-            className={`w-full max-w-[32rem] mx-auto h-[100vh]`}
+            className={`w-full max-w-[32rem] mx-auto h-[100vh] ${className}`}
             style={{
                 background: isBackgroundColor ? 
                 "linear-gradient(180deg, #FEFEFE 16%, #a8e5d1a0 63%, #50cca377 93%, #10b9816b 100%)" : "auto",
