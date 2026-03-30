09714bb24aa2f9b194296bf846af7d995258733f
songhajang <angpan2004@gmail.com>
2026-03-30 17:58:37 +0900

    [feat] #7 - 카테고리 / 폴더 api 연동 함수 생성
    - TODO : api 연동 코드 적용 예정

---
+++ src/util/getCategoryList.ts
@@ -0,0 +1,5 @@
+export const getCategoryList = () =>{
+    //TODO: 카테고리 불러오는 api 연동 예정
+    const data = ["카페", "레스토랑", "공원", "도서관","박물관", "쇼핑몰", "병원", "헬스장",];
+    return data;
+}
---
+++ src/util/getFolderList.ts
@@ -0,0 +1,5 @@
+export const getFolderList = () =>{
+    //TODO: folder리스트 받아오는 api 연동 예정
+    const data = ["폴더 1", "폴더 2", "폴더 3" ];
+    return data;
+}
