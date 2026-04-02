16ec86aa382701966277bd86d6feb5fc4afb6a36
songhajang <angpan2004@gmail.com>
2026-04-02 17:08:14 +0900

    [refactor] #7 - home 와이어프레임 수정

--- src/Pages/HomeView.tsx
+++ src/Pages/HomeView.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+import FolderCard from "../components/folder/FloderCard";
 import CreateFolderCard from "../components/home/CreateFolderCard";
 import DefaultMain from "../components/layout/DafultMain";
+import type { FolderType } from "../types/FolderTypes";
+import { FOLDER_ITEMS } from "../data/FolderList";
 
 const HomeView = () => {
-
-    const users = [1, 2, 3, 4]
+    const [folderList , setFolderList] = useState<FolderType[]>(FOLDER_ITEMS);
 
     const places = [1, 2, 3]
 
@@ -12,54 +15,13 @@ const HomeView = () => {
             <div className="p-5">
                 <CreateFolderCard />
 
-                <div className="bg-gray-100 rounded-xl p-4">
-
-                    <div className="flex justify-between mb-3">
-                        <span className="font-medium">추천 팔로우</span>
-                        <span className="text-xs text-gray-500">&lt;1/5&gt;</span>
-                    </div>
-
-                    <div className="flex gap-3">
-                        {users.map((_, i) => (
-                            <div className="w-14 h-14 bg-gray-200 rounded-lg" key={i} />
-                        ))}
-                    </div>
-
-                </div>
-
-                <div>
-
+                <div className="mt-5">
                     <h2 className="font-semibold mb-3">
                         장소 추천 TOP 10
                     </h2>
 
                     <div className="space-y-3">
-                        {places.map((_, i) => (
-                            <div key={i} className="relative rounded-xl overflow-hidden">
-
-                                <img
-                                    src="/test.jpg"
-                                    className="w-full h-40 object-cover"
-                                />
-
-                                <div className="absolute bottom-3 left-3 text-white">
-
-                                    <p className="font-semibold">
-                                        폴더명
-                                    </p>
-
-                                    <p className="text-xs opacity-80">
-                                        폴더 간단 설명
-                                    </p>
-
-                                    <p className="text-xs opacity-70 mt-1">
-                                        최근 업데이트 2025.05.10
-                                    </p>
-
-                                </div>
-
-                            </div>
-                        ))}
+                        {folderList.map((element, i) => ( <FolderCard key={i} folder={element} />))}
                     </div>
 
                 </div>
